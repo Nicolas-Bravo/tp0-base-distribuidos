c75ef8e71bc84f7d23b7671d83e43eb91559fcb8
Nicolas Bravo <bravo.nicolasf@gmail.com>
2026-03-24 14:53:40 -0300

    ej3

---
+++ validar-echo-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+MESSAGE="test-echo-$(date +%s)"
+NETWORK="tp0_testing_net"
+SERVER_SERVICE="server"
+
+# Envío el mensaje desde un contenedor auxiliar con netcat
+RESPONSE=$(docker run --rm --network=${NETWORK} alpine /bin/sh -c "echo '$MSG' | nc ${SERVER_SERVICE} 12345")
+
+if [ "${RESPONSE}" = "${MESSAGE}" ]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
+
+  exit 0
